1623ebb544a42486d87b9a1ba025cb49895c3ab4
huahua132 <958677003@qq.com>
2025-06-27 12:53:20 +0800

    netpack

---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Module/NetPackModule/RpcNetPackage.cs
@@ -0,0 +1,51 @@
+using TEngine;
+namespace GameLogic
+{
+    /// <summary>
+    /// 对接skynet_fly rpc 数据包实现。https://github.com/huahua132/skynet_fly
+    /// 用于在网络通信中封装基础消息结构。
+    /// </summary>
+    /// <remarks>
+    /// 实现 <see cref="INetPackage"/> 接口，提供消息ID与二进制载荷的标准容器。
+    /// 通常配合编解码器（Encoder/Decoder）进行序列化与反序列化操作。
+    /// </remarks>
+    public class RpcNetPackage : INetPackage
+    {
+        public enum PACK_TYPE
+        {
+            WHOLE = 0,  //整包
+            HEAD = 1,  //包头
+            BODY = 2,  //包体
+            TAIL = 3,  //包尾
+        }
+        public enum MSG_TYPE
+        {
+            SERVER_PUSH = 0,
+            CLIENT_PUSH = 1,
+            CLIENT_REQ = 2,
+            SERVER_RSP = 3,
+            SERVER_ERR = 4,
+        }
+        /// <summary>
+        /// 包类型，字段描述: (0-整包  1包头  2包体  3包尾)
+        /// </summary>
+        public byte packtype { set; get; }
+        /// <summary>
+        /// 消息类型，字段描述: (0-服务端推送 1-客户端推送 2-客户端请求 3-服务端回复 4-服务器回复错误)
+        /// </summary>
+        public byte msgtype { set; get; }
+        /// <summary>
+        /// 协议码
+        /// </summary>
+        public ushort packid { set; get; }
+        /// <summary>
+        /// 会话号 字段描述: (服务端推送时用于标识同一包体，客户端推送为0即可(不能发送大包)，客户端请求(奇数)达到(4,294,967,295)时客户端应该直接切换到1,避免服务端使用溢出后的0进行回复, 服务端回复，服务器回复错误(偶数,奇数基础上1))
+        /// </summary>
+        public uint session { set; get; }
+
+        /// <summary>
+        /// 消息内容 字段描述：包头时为4字节的消息内容长度
+        /// </summary>
+        public byte[] msgbody { set; get; }
+    }
+}
---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Module/NetPackModule/RpcNetPackageDecoder.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using TEngine;
+
+namespace GameLogic
+{
+    /// <summary>
+    /// 网络包解码器。
+    /// </summary>
+    public class RpcNetPackageDecoder : INetPackageDecoder
+    {
+        private HandleErrorDelegate _handleErrorCallback;
+        private const int HeaderFiledsSize = 8; //包头里其他字段占用8字节
+        private const int HeaderMsgBodyLengthFiledSize = 2; //包头里的包体长度（ushort类型）。
+
+        /// <summary>
+        /// 获取包头的尺寸。
+        /// </summary>
+        public int GetPackageHeaderSize()
+        {
+            return HeaderFiledsSize + HeaderMsgBodyLengthFiledSize;
+        }
+
+        /// <summary>
+        /// 注册异常错误回调方法。
+        /// </summary>
+        public void RegisterHandleErrorCallback(HandleErrorDelegate callback)
+        {
+            _handleErrorCallback = callback;
+        }
+
+        /// <summary>
+        /// 网络消息解码。
+        /// </summary>
+        /// <param name="packageBodyMaxSize">包体的最大尺寸。</param>
+        /// <param name="ringBuffer">解码需要的字节缓冲区。</param>
+        /// <param name="outputPackages">接收的包裹列表。</param>
+        public void Decode(int packageBodyMaxSize, RingBuffer ringBuffer, List<INetPackage> outputPackages)
+        {
+            // 循环解包。
+            while (true)
+            {
+                // 如果数据不够一个包头。
+                if (ringBuffer.ReadableBytes < GetPackageHeaderSize())
+                    break;
+                ringBuffer.MarkReaderIndex();
+                // 读取消息包长度
+                ushort msgsz = ringBuffer.ReadUShort();
+
+                // 如果剩余可读数据小于包体长度。
+                if (ringBuffer.ReadableBytes < msgsz)
+                {
+                    ringBuffer.ResetReaderIndex();
+                    break; //需要退出读够数据再解包。
+                }
+
+                RpcNetPackage package = new RpcNetPackage();
+                package.packtype = ringBuffer.ReadByte(); //包类型
+                package.msgtype = ringBuffer.ReadByte();  //消息类型
+                package.packid = ringBuffer.ReadUShort(); //协议号
+                package.session = ringBuffer.ReadUInt();  //会话号
+
+                int msgBodyLength = msgsz - HeaderFiledsSize;
+
+                // 检测包体长度。
+                if (msgBodyLength > packageBodyMaxSize)
+                {
+                    _handleErrorCallback(true, $"The decode package {package.packid} body size is larger than {packageBodyMaxSize} !");
+                    break;
+                }
+
+                // 读取包体。
+                {
+                    package.msgbody = ringBuffer.ReadBytes(msgBodyLength);
+                    outputPackages.Add(package);
+                }
+            }
+
+            // 注意：将剩余数据移至起始。
+            ringBuffer.DiscardReadBytes();
+        }
+    }
+}
